e798b5784969670feb3a6ca365cb74ebc9b56be2
Arshad <arshadakjsa@gmail.com>
2021-03-06 15:53:39 +0530

    main python file

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ class TicTacToe(MDApp):
         index = self.box_index[identity]
 
         if self.matrix_equivalent[int(index[0])][int(index[1])] == '0':
-
-            if self.number_press % 2 == 0:
+            self.number_press += 1
+            if self.number_press % 2 != 0:
                 self.root.window.get_screen('normal').ids[identity].text = 'X'
                 self.root.window.get_screen('normal').ids[identity].color = (84 / 255, 84 / 255, 84 / 255, 1)
                 self.root.window.get_screen('normal').ids.turn.text = 'O  Turn'
@@ -52,6 +52,7 @@ class TicTacToe(MDApp):
                 self.root.window.get_screen('normal').ids[identity].color = (242 / 255, 235 / 255, 211 / 255, 1)
                 self.root.window.get_screen('normal').ids.turn.text = 'X  Turn'
                 self.matrix_equivalent[int(index[0])][int(index[1])] = 'O'
+
         if self._is_win():
             self.root.current = 'win'
             if self._is_win() == 'X':
@@ -59,27 +60,31 @@ class TicTacToe(MDApp):
 [b][size=200sp][color=#545454]{self._is_win()}[/size][/b]
 [size=80sp]WINNER![/size][/color]
 '''
+
                 self.x_score += 1
                 self.root.window.get_screen(
                     'normal').ids.score_x.text = f'X                                                    {self.x_score}'
+
             else:
                 self.root.window.get_screen('win').ids.win.text = f'''
 [b][size=200sp]{self._is_win()}[/size][/b]
 [size=80sp][color=#545454]WINNER![/size][/color]
 '''
+
                 self.o_score += 1
                 self.root.window.get_screen(
                     'normal').ids.score_o.text = f'O                                                    {self.o_score}'
 
             self.restart_game()
             return
-        self.number_press += 1
+        
         if self.number_press == 9:
             self.root.current = 'win'
             self.root.window.get_screen('win').ids.win.text = f'''
 [b][size=200sp][color=#545454]X[/size][/b][/color] O
 [size=80sp][color=#545454]DRAW![/size][/color]
 '''
+
             self.restart_game()
             return
 
